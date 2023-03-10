e6aeded88e2b9a07198b13cc1ab0e9848d67c7b4
Connie Jones <sway.jones@cox.net>
2023-03-09 21:33:01 -0600

    2023-January-Foundations

---
+++ first-javascript/script.js
@@ -0,0 +1,90 @@
+// console.log("Hello");
+
+// var book = "Dracula";
+// var movie;
+// console.log(book);
+// console.log(movie);
+// movie = "interview with a Vampire";
+// console.log(movie);
+
+// var count = "3 ";
+// var num = "2";
+// console.log(count + num);
+// console.log(5 + 8);
+// console.log("5" + "8");
+// console.log(5 + "8");
+
+// var firstName = "Dan";
+// var lastName = "Taylor";
+// console.log(firstName + " " + lastName);
+
+// // var cars = ["Subaru", "Audi", "Volkswagen"];
+// // console.log("The third car in the list is " + cars [2]);
+// // cars.push("Chevy");
+// // cars.push("Ford");
+// // cars.push("Honda");
+// // cars.push("Toyota");
+// // cars.push("Fiat");
+// // cars.push("Buick");
+// // cars.push("Jeep");
+// // console.log(cars.length);
+// // // ["Subaru", "Audi", "Voltswagen", "Chevy", "Ford", "Honda", "Toyota",
+// // // "Fiat", "Buick", "Jeep"]
+// // console.log(cars);
+// // console.log(cars[cars.length-1]);
+// // cars.pop();
+// // console.log(cars);
+// // console.log(cars.length);
+// // console.log(cars[cars.length-1]);
+// // cars.splice(2, 3);
+// // console.log(cars);
+// // console.log(cars.length);
+
+// // var student = {
+// //     firstName: "Henry",
+// //     lastName: "Jackson",
+// //     id: 12345,
+// //     courses: [
+// //         {
+// //             class: "Full-Stack Jaava",
+// //             instructor: {
+// //                 firstName: "Steve"
+// //                 lastName: "Wozniak" 
+// //             },
+// //             gradePercentaage: 97
+// //         },
+// //         {
+// //             class: "Calculus",
+// //             instructor: {
+// //                 firstName: "Bill",
+// //                 lastName: "Gates"
+// //             },
+// //             gradePercentage: 86
+// //         }
+// //     ]
+// // }
+
+// // console.log(' My name is '  + student.firstName + ' ' + student.lastName + ' and I am taking ' + student.student.courses);
+
+// var zombies = 2;
+// if (zombies > 10) {
+//     console.log('AAAAARRRRGGGGG ZOMBIES!!!!');
+// } else if (zombies > 5) {
+//     console.log('Gotta stay away from those zombies!');
+// } else {
+//     console.log('Not a lot of zombies today.');
+// }
+
+// var apples = 15;
+// if (apples > 3) {
+//     console.log('Let us make tarts!');
+// } else if (apples >10) {
+//     console.log('Let us make a pie! We can use the "Betty Crocker" cookbook!');
+// }
+
+var numOfStudents = 13;
+if (numOfStudents > 20) {
+    console.log("Too many students");
+} else if (numOfStudents > 15) {
+    console.log("Good number of students");
+}
