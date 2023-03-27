a89a02508e7bdf681bfeedb06bffc78073eb0a5a
Connie Jones <sway.jones@cox.net>
2023-03-27 09:50:10 -0500

    2023-January-Foundations

---
+++ Class7-Homework/sandbox.js
@@ -0,0 +1,11 @@
+// alert('hello, world');
+// console.log(1);
+// var fName = 'Connie'
+// var lName = 'Jones'
+// console.log(fName, lName);
+// console.log('hello, world');
+
+let firstName = 'brandon';
+let lastName = 'Sanderson';
+let fullname = firstName + ' ' + lastName;
+console.log(fullname);
